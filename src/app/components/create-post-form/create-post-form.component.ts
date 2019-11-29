e0b607b6a4c7a827fc3735fc7377d58226831f2b
DWS-paris <j.noyer@digitalworkshop.fr>
2019-11-29 10:08:02 +0100

    Création du formulaire

--- src/app/components/create-post-form/create-post-form.component.ts
+++ src/app/components/create-post-form/create-post-form.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+/* 
+Imports & definition 
+*/
+  import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-create-post-form',
-  templateUrl: './create-post-form.component.html',
-  styles: []
-})
-export class CreatePostFormComponent implements OnInit {
+  // Gestion des formulaires
+  import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-  constructor() { }
+  @Component({
+    selector: 'app-create-post-form',
+    templateUrl: './create-post-form.component.html'
+  })
+//
 
-  ngOnInit() {
+/* 
+Export
+*/
+  export class CreatePostFormComponent implements OnInit {
+
+    /* 
+    Declaration
+    */
+      public form: FormGroup;
+    //
+    constructor( private FormBuilder: FormBuilder ) { }
+
+    /* 
+    Methods
+    */
+      private resetForm = () => {
+        // Configurer le formulaire
+        this.form = this.FormBuilder.group({
+          title: [ undefined, Validators.required ],
+          body: [ undefined, Validators.required ]
+        });
+      }
+    //
+
+    ngOnInit() {
+    }
+
   }
-
-}
+//
--- src/app/components/create-post-form/module.ts
+++ src/app/components/create-post-form/module.ts
@@ -4,6 +4,9 @@ Imports
     import { NgModule } from "@angular/core";
     import { CommonModule } from "@angular/common";
     import { CreatePostFormComponent } from "./create-post-form.component";
+
+    // Gestion des formulaires
+    import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //
 
 /* 
@@ -12,7 +15,11 @@ Definition & export
     @NgModule({
         declarations: [ CreatePostFormComponent ],
         exports: [ CreatePostFormComponent ],
-        imports: [ CommonModule ]
+        imports: [ 
+            CommonModule, 
+            FormsModule, 
+            ReactiveFormsModule 
+        ]
     })
 
     export class CreatePostFormModule{};
